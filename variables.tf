ece80eeb60798ac5342f1dda4e9e67257e60c52f
y-ohgi <ohgi.211@gmail.com>
2019-06-26 07:45:19 +0900

    initial components

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  default     = "myapp"
+}
+
+variable "description" {
+  description = "Name to be used on all the resources as identifier"
+  default     = ""
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  default     = {}
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where to create security group"
+  default     = ""
+}
+
+variable "ingress" {
+  description = "List of ingress rules. (Simplified)"
+  default     = []
+}
+
+variable "ingress_with_cidr_block_rules" {
+  description = "List of ingress rules"
+  default     = []
+}
